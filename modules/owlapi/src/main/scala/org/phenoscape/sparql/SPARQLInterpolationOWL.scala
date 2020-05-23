cb90a16f0a4ea6e21b06d5ca4c7d530cc95cc461
Jim Balhoff <jim@balhoff.org>
2020-05-23 10:58:41 -0400

    Add OWL API test.

--- modules/owlapi/src/main/scala/org/phenoscape/sparql/SPARQLInterpolationOWL.scala
+++ modules/owlapi/src/main/scala/org/phenoscape/sparql/SPARQLInterpolationOWL.scala
@@ -1,12 +1,15 @@
 package org.phenoscape.sparql
 
 import contextual._
+import org.apache.jena.datatypes.{RDFDatatype, TypeMapper}
+import org.apache.jena.graph.NodeFactory
 import org.apache.jena.query.ParameterizedSparqlString
+import org.apache.jena.rdf.model.ResourceFactory
 import org.phenoscape.sparql.SPARQLInterpolation.SPARQLInterpolator.SPARQLContext
 import org.phenoscape.sparql.SPARQLInterpolation._
-import org.semanticweb.owlapi.model.{IRI, OWLAnnotationProperty, OWLClass, OWLObjectProperty}
+import org.semanticweb.owlapi.model.{IRI, OWLAnnotationProperty, OWLClass, OWLDataProperty, OWLLiteral, OWLObjectProperty}
 
-object SPARQLInterpolatorOWLAPI {
+object SPARQLInterpolationOWL {
 
   implicit val embedIRIInSPARQL = SPARQLInterpolator.embed[IRI](Case(SPARQLContext, SPARQLContext) { iri =>
     val pss = new ParameterizedSparqlString()
@@ -29,6 +32,20 @@ object SPARQLInterpolatorOWLAPI {
   implicit val embedOWLAnnotationPropertyInSPARQL = SPARQLInterpolator.embed[OWLAnnotationProperty](Case(SPARQLContext, SPARQLContext) { obj =>
     val pss = new ParameterizedSparqlString()
     pss.appendIri(obj.getIRI.toString)
+    pss.toString
+  })
+
+  implicit val embedOWLDataPropertyInSPARQL = SPARQLInterpolator.embed[OWLDataProperty](Case(SPARQLContext, SPARQLContext) { obj =>
+    val pss = new ParameterizedSparqlString()
+    pss.appendIri(obj.getIRI.toString)
+    pss.toString
+  })
+
+  implicit val embedOWLLiteralInSPARQL = SPARQLInterpolator.embed[OWLLiteral](Case(SPARQLContext, SPARQLContext) { literal =>
+    val pss = new ParameterizedSparqlString()
+    if (literal.hasLang) pss.appendLiteral(literal.getLiteral, literal.getLang)
+    else if (literal.isRDFPlainLiteral) pss.appendLiteral(literal.getLiteral)
+    else pss.appendLiteral(literal.getLiteral, TypeMapper.getInstance.getSafeTypeByName(literal.getDatatype.getIRI.toString))
     pss.toString
   })
 
